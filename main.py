51324ed97ad7a51eff3df2e587eee7a1da479c46
Ignat M <gntshka@gmail.com>
2025-07-16 13:08:53 +0500

    16_07_2025

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('Hello world')
+import telebot
+import configparser
+
+def read_config(path):
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+token = read_config('settings.ini')
+bot = telebot.TeleBot(token['Token']['telegram_token'])
+
+@bot.message_handler(commands=['start'])
+def bot_start(message):
+    bot.send_message(message.chat.id, 'Правила просты, выбирай правильный перевод слова и жми "Далее"')
+    russian_word = 'Someone'
+    target_word = 'Someone'
+
+
+
+
+
+if __name__ == '__main__':
+    print('Hi')
+    bot.polling()
